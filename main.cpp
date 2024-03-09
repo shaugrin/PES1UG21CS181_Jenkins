457342adce77dd9664fff9834fa30d42bc225724
Digvijay Sunil <93257010+shaugrin@users.noreply.github.com>
2024-03-09 22:46:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,10 @@
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                sh 'mvn clean install'
-                echo 'Build Stage Successful'
-            }
-        }
-        stage('Test') {
-            steps {
-                sh 'mvn test'
-                echo 'Test Stage Successful'
-                post {
-                    always {
-                        junit 'target/surefire-reports/*.xml'
-                    }
-                }
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sh 'mvn deploy'
-                echo 'Deployment Successful'
-            }
-        }
-    }
-    post {
-        failure {
-            echo 'Pipeline failed'
-        }
-    }
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Jenkin bhai\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
 }
